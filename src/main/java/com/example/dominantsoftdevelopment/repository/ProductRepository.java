bc6681f6aad05dd2a6ef00707275b6c20fc813a8
Nodirbek <no0404ir@gmail.com>
2024-04-29 21:34:40 +0500

    add statistika

--- src/main/java/com/example/dominantsoftdevelopment/repository/ProductRepository.java
+++ src/main/java/com/example/dominantsoftdevelopment/repository/ProductRepository.java
@@ -2,16 +2,26 @@ package com.example.dominantsoftdevelopment.repository;
 
 import com.example.dominantsoftdevelopment.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByDeletedFalse();
+
     Optional<Product> findByIdAndDeletedFalse(Long id);
+
     List<Product> findBySellerIdAndDeletedFalse(Long seller_id);
+
     List<Product> findBySellerIdAndDeletedTrue(Long seller_id);
+
+    @Query(nativeQuery = true, value = """
+                 select year,count(*) from
+                 (select extract(year from p.updated_at) as year from product p) as ss
+                 group by ss.year
+                    """)
+    List<Product> getProductYear();
 }
